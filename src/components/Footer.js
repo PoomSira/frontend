33a6ff0cf35dc501c9f5ccd458cca9e74f7b2601
Siramade Apivirasirikul <siravira08@gmail.com>
2024-08-06 00:16:18 +1000

    Edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,15 @@ const Footer = () => {
         <p>
           &copy; 2024 Cyclink Monash University IE Group Project TA30. All
           rights reserved.
+          <br />
+          Cyclink respectfully acknowledges the Traditional Owners of the land
+          we govern, the Wurundjeri Woi-wurrung and Bunurong / Boon Wurrung
+          peoples of the Kulin and pays respect to their Elders past and
+          present. We acknowledge and honour the unbroken spiritual, cultural
+          and political connection they have maintained to this unique place for
+          more than 2000 generations. We accept the invitation in the Uluru
+          Statement from the Heart and are committed to walking together to
+          build a better future.
         </p>
       </div>
     </footer>
